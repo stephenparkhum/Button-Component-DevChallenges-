56737dee14bbf3cb0553c6334c664aa57622a5ed
Stephen <stephenparkhum@outlook.com>
2020-09-12 08:28:54 -0700

    initial commit for setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside>
+        <h3>DevChallenges</h3>
+        <ul className="NavList">
+          <li>
+            Button
+          </li>
+        </ul>
+      </aside>
+      <main>
+        Main here
+      </main>
+    </div>
+  );
+}
+
+export default App;
